7553c07ebc99088f0f4d532d6095de37a7d232bf
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2024-09-11 01:33:20 +0900

    10.1

---
+++ pages/SiteGPT.py
@@ -0,0 +1,36 @@
+from langchain.document_loaders import WebBaseLoader
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
+import streamlit as st
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+html2text_transformer = Html2TextTransformer()
+
+st.markdown(
+    """
+    # SiteGPT
+            
+    Ask questions about the content of a website.
+            
+    Start by writing the URL of the website on the sidebar.
+"""
+)
+
+
+with st.sidebar:
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
+
+if url:
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(transformed)
+    # https://openai.com/index/introducing-gpts/
